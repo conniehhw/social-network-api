fb73ebc9e5a57fea142caeadfce8ad7ca5331334
Connie Wong <conniehhw@gmail.com>
2023-02-11 17:26:13 -0500

    Install libraries set up dB connection

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const { connect, connection } = require('mongoose');
+
+//a way to connect to database and export out that object; const db = require('./config/connection');
+connect('mongodb://127.0.0.1:27017/userThoughts', { //URI connection object or can be ://localhost, then dB name
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
